81c0f1cb8a02d44491926b87fad234e2432864d5
Andy Ouyang <andy.ouyang@stonybrook.edu>
2019-04-25 09:52:07 -0400

    changed it back

--- queue.py
+++ queue.py
@@ -1,9 +1,7 @@
-import pika
-import sys
-import json
+import pika                                                                           
+import sys                                                                            
+import json                                                               
 import pymongo
-import threading
-import functools
 
 myclient = pymongo.MongoClient('mongodb://130.245.170.88:27017/')
 mydb = myclient['finalproject']
@@ -11,47 +9,28 @@ users = mydb['users']
 questions = mydb['questions']
 answers = mydb['answers']
 
-def do_work(connection, channel, delivery_tag, body):
-    thread_id = threading.get_ident()
-    # fmt1 = 'Thread id: {} Delivery tag: {} Message body: {}'
-    # LOGGER.info(fmt1.format(thread_id, delivery_tag, body))
-    # Sleeping to simulate 10 seconds of work
-    # time.sleep(10)
-    body = body.decode('utf-8')                                   
-    doc = json.loads(body)
-    collection = None
-    if doc['collection'] == 'questions':
-        collection = questions
-    elif doc['collection'] == 'users':
-        collection = users
-    elif doc['collection'] == 'answers':
-        collection = answers
-    collection.insert_one(doc)
-    cb = functools.partial(ack_message, channel, delivery_tag)
-    connection.add_callback_threadsafe(cb)
-
-def dequeue():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.exchange_declare('mongodb', 'direct')
-    exc = channel.queue_declare(queue='mongo', durable=True)
-    channel.queue_bind(exchange='mongodb', queue='mongo', routing_key='mongo')
-    threads = []
-    on_message_callback = functools.partial(on_message, args=(connection, threads))
-    channel.basic_consume(on_message_callback, 'standard')
-
-    try:
-        channel.start_consuming()
-    except KeyboardInterrupt:
-        channel.stop_consuming()
-
-    # Wait for all to complete
-    for thread in threads:
-        thread.join()
-
-    connection.close()
-	    #channel.basic_ack()
-
-dequeue()
-
-
+def dequeue():                                                                        
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))  
+    channel = connection.channel()                                                
+    channel.exchange_declare('mongodb', 'direct')                                 
+    exc = channel.queue_declare(queue='mongo', durable=True)                      
+    channel.queue_bind(exchange='mongodb', queue='mongo', routing_key='mongo')    
+    print('listening')                                                            
+    while True:                                                                   
+        meth, prop, body = channel.basic_get(queue='mongo')                   
+        if body is not None:                                                  
+            #channel.close()                                              
+            #connection.close()                                           
+            body = body.decode('utf-8')                                   
+            doc = json.loads(body)
+            collection = None
+            if doc['collection'] == 'questions':
+                collection = questions
+            elif doc['collection'] == 'users':
+                collection = users
+            elif doc['collection'] == 'answers':
+                collection = answers
+            collection.insert_one(doc)
+            print("got message: " + str(doc), sys.stderr)
+                                                                                      
+dequeue()                                                                             
