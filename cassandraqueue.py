c5fab0d0264905edd32d34a9e741f9a5c979684b
Andy Ouyang <andy.ouyang@stonybrook.edu>
2019-04-25 12:12:57 -0400

    added cassandra queue

---
+++ cassandraqueue.py
@@ -0,0 +1,47 @@
+import pika
+import sys
+import json
+import pymongo
+
+# myclient = pymongo.MongoClient('mongodb://130.245.170.88:27017/')
+# mydb = myclient['finalproject']
+# users = mydb['users']
+# questions = mydb['questions']
+# answers = mydb['answers']
+
+cluster = Cluster(['130.245.171.50'])
+session = cluster.connect(keyspace='stackoverflow')
+
+def callback(ch, method, properties, body):
+    body = body.decode('utf-8')
+    info = body.split(',')
+    media_id = info[0]
+    b = bytearray(info[1])
+    filetype = info[2]
+    added = True if info[3] == 'True' else False
+    username = info[4]
+    cqlinsert = 'insert into media (id, content, type, added, poster) values (%s, %s, %s, %s, %s);'
+    session.execute(cqlinsert, (media_id, b, filetype, added, username))
+    # session.execute(body)
+    # doc = json.loads(body)
+    # collection = None
+    # if doc['collection'] == 'questions':
+    #     collection = questions
+    # elif doc['collection'] == 'users':
+    #     collection = users
+    # elif doc['collection'] == 'answers':
+    #     collection = answers
+    # collection.insert_one(doc)
+    print("got message: " + cqlinsert, sys.stderr)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+def dequeue():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    # channel.exchange_declare('mongodb', 'direct')
+    exc = channel.queue_declare(queue='cassandra', durable=True)
+    # channel.queue_bind(exchange='mongodb', queue='mongo', routing_key='mongo')
+    print('listening')
+    channel.basic_consume(queue='cassandra', on_message_callback=callback)
+    channel.start_consuming()
+dequeue()                     
